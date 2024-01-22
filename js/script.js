189a8b7fd443e27389b0828b24d4c55719bfb27d
lucasgrolli13@gmail.com <lucasgrolli13@gmail.com>
2024-01-21 21:26:32 -0300

    add_shiny_button

--- js/script.js
+++ js/script.js
@@ -7,8 +7,11 @@ const input = document.querySelector('.input_search');
 
 const buttonPrev = document.querySelector('.btn-prev');
 const buttonNext = document.querySelector('.btn-next');
+const buttonShiny = document.querySelector('.btn-shiny');
+
 
 let searchPokemon = 1;
+let isShiny = false;
 
 const fetchPokemon = async (pokemon) => {
     const APIReponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
@@ -30,10 +33,18 @@ const renderPokemon = async (pokemon) => {
     const data = await fetchPokemon(pokemon);
 
     if (data) {
+        updateShinyState();
         pokemonImage.style.display = 'block';
         pokemonName.innerHTML = data.name;
         pokemonNumber.innerHTML = data.id;
-        pokemonImage.src = data['sprites']['versions']['generation-v']['black-white']['animated']['front_default'];
+
+        if(isShiny){
+            pokemonImage.src = data['sprites']['versions']['generation-v']['black-white']['animated']['front_shiny'];
+        }
+        else{
+            pokemonImage.src = data['sprites']['versions']['generation-v']['black-white']['animated']['front_default'];
+        }
+
         input.value = '';
         searchPokemon = data.id;
     }
@@ -46,6 +57,24 @@ const renderPokemon = async (pokemon) => {
         input.value = ''
     }
 }
+
+
+const updateShinyState = () => {
+
+    if (isShiny){
+        buttonShiny.classList.add('shiny');
+    }
+    else{
+        buttonShiny.classList.remove('shiny');
+    }
+}
+
+buttonShiny.addEventListener('click', () => {
+    isShiny = !isShiny;
+    updateShinyState();
+    renderPokemon(searchPokemon);
+})
+
 
 form.addEventListener('submit', (event) => {
     event.preventDefault();
